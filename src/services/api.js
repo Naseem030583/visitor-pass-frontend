5a919b869778465231b832d9e9a019428a495b4d
Naseem030583 <naseem.ahamad.du@gmail.com>
2026-02-22 19:40:08 -0600

    Updated API URL for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://visitor-pass-backend-azj3.onrender.com/api'
 });
 
 // Add token to every request automatically
